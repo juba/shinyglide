d32c4b8ebb6659c3f66093fa8f3cdc7ba64d2fc9
Julien <julien@nozav.org>
2019-06-07 10:55:04 +0200

    Rework modal sample app

--- inst/examples/04_custom_controls/app.R
+++ inst/examples/04_custom_controls/app.R
@@ -5,22 +5,33 @@ library(shinyglide)
 
 controls <- tags$div(
     tags$div(class="my-control prev-screen"),
-    tags$div(class="my-control next-screen")
+    tags$div(class="my-control next-screen"),
+    div(`data-glide-el`="controls",
+      tags$a(
+        class="my-control last-screen",
+        `data-glide-dir` = "<<",
+        icon("repeat", lib = "glyphicon")
+      )
+    )
   )
 
 css <- "
 .container-fluid {
-  max-width: 500px;
+  max-width: 700px;
+  padding: 0 20px;
 }
 .shinyglide {
-  border: 1px solid black;
+  border: 1px solid #888;
+  box-shadow: 0px 0px 20px #888;
 }
 .my-control {
+  display: block;
   position: absolute;
   top: 50%;
   transform: translateY(-50%);
   line-height: 1;
   font-size: 2.5em;
+  color: #0055DD;
   opacity: 0.8;
   cursor: pointer;
 }
@@ -32,13 +43,30 @@ css <- "
   cursor: not-allowed;
 }
 .prev-screen {
-  left: 10px;
+  left: 20px;
 }
-.next-screen {
-  right: 10px;
+.next-screen,
+.last-screen {
+  right: 20px;
 }
 .glide__slides {
-  margin: 0 4em;
+  margin: 0 7em;
+}
+
+@keyframes hourglass {
+  from {
+    transform: translateY(-50%) rotate(0deg);
+  }
+  50% {
+    transform: translateY(-50%) rotate(359deg);
+  }
+  to {
+    transform: translateY(-50%) rotate(360deg);
+  }
+}
+.loading {
+  font-size: 2em;
+  animation: hourglass 1s linear infinite;
 }
 "
 
@@ -47,33 +75,34 @@ ui <- fluidPage(
     tags$style(css)
   ),
   titlePanel("shinyglide custom controls app"),
-
+  fluidRow(
+    p("This simple app shows an example of completely custom controls.")
+  ),
   fluidRow(
       glide(
-        next_label = icon("circle-arrow-right", lib="glyphicon"),
-        previous_label = icon("circle-arrow-left", lib="glyphicon"),
+        next_label = icon("chevron-right", lib="glyphicon"),
+        previous_label = icon("chevron-left", lib="glyphicon"),
         loading_label = icon("hourglass", lib="glyphicon"),
         height = "300px",
         custom_controls = controls,
 
         screen(
-          h3("Custom controls"),
-          p(HTML("First screen"))
+          h3("First screen"),
+          p("Click the arrow to go to next screen")
         ),
 
         screen(
-          h3("Custom controls"),
-          p(HTML("Second screen with loading")),
-          checkboxInput("x", "x", value = FALSE)
+          h3("Second screen"),
+          p("Check the following checkbox to see the loading animation."),
+          checkboxInput("x", "Such wow !", value = FALSE)
         ),
 
         screenOutput("loading_screen"),
 
         screen(
-          h3("Custom controls"),
-          p(HTML("Final screen"))
-        )
-
+          h3("Final screen"),
+            p("And... this is the end.")
+          )
       )
   )
 )
@@ -83,12 +112,16 @@ server <- function(input, output, session) {
 
   output$loading_screen <- renderUI({
     Sys.sleep(2)
-    input$x
     list(
       h3("Custom controls"),
-      p(HTML("Loaded screen"))
+      if(input$x) {
+        p("Incredible, you checked the box !")
+      } else {
+        p("You unchecked the box, unbelievable !")
+      }
     )
   })
+  outputOptions(output, "loading_screen", suspendWhenHidden = FALSE)
 
 }
 
